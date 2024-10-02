0652a061124c3cb7d2fa24121ca92e026d6e0302
Lukáš Trkan <trkan@wpj.cz>
2024-10-02 14:23:14 +0200

    Initial commit

---
+++ src/main/java/com/example/cars/CarsDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.cars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarsDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarsDatabaseApplication.class, args);
+	}
+
+}
